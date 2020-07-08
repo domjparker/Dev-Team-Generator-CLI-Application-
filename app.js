e6035998b5f1330d0b46d6e4b0252b52278ca89b
Rees Dart <thedominicparker@gmail.com>
2020-07-08 16:36:30 -0700

    all team member functions added.

--- app.js
+++ app.js
@@ -9,6 +9,142 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
+const teamArray = [];
+
+function addTeamMember() {
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "addEmployeeYesNo",
+        choices: ["Add a manager to team","Add an engineer to team","Add an intern to team","Get the team HTML file","Quit"]
+    }).then(function ({ addEmployeeYesNo }) {
+        switch (addEmployeeYesNo) {
+            case "Add a manager to team":
+                addManager();
+                break;
+            case "Add an engineer to team":
+                addEngineer();
+                break;
+            case "Add an intern to team":
+                addIntern();
+                break;
+            case "Get team HTML file":
+                getTeamFile(outputPath,render(teamArray));
+                break;
+            default:
+                console.log("This has ended the application."); 
+                break;
+        }
+    })
+};
+
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is this manager's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is this manager's id#?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "what is this manager's email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "what is this manager's office number?",
+            name: "officeNumber"
+        }
+    ]).then(function (addManager) {
+        const thisManager = new Manager(addManager);
+        teamArray.push(thisManager);
+        console.log(`${addManager.name} has been added as a manager. Your team now has ${teamArray.length} member(s).`);
+        addTeamMember()
+    })
+};
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is this engineer's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is this engineer's id#?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "what is this engineer's email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is this engineer's Github username?",
+            name: "github"
+        }
+    ]).then(function (addEngineer) {
+        const thisEngineer = new Engineer(addEngineer);
+        teamArray.push(thisEngineer);
+        console.log(`${addEngineer.name} has been added as a manager. Your team now has ${teamArray.length} member(s).`);
+        addTeamMember()
+    })
+};
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is this intern's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is this intern's id#?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "what is this intern's email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What school does this intern attend?",
+            name: "school"
+        }
+    ]).then(function (addIntern) {
+        const thisIntern = new Intern(addIntern);
+        teamArray.push(thisIntern);
+        console.log(`${addIntern.name} has been added as a manager. Your team now has ${teamArray.length} member(s).`);
+        addTeamMember()
+    })
+};
+
+function getTeamFile(outputPath,renderTeam) {
+    if(!teamArray.length) {
+        console.log("First, you need to add members to the team.");
+        addTeamMember();
+    }
+    else {
+        
+    }
+};
+
+addTeamMember()
+
+
+
+
+
 
 
 // Write code to use inquirer to gather information about the development team members,
